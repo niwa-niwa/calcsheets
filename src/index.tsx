13f16141a7271bb925e12db5da29b3208bcb7ec7
niwa-niwa <urban.hang274@gmail.com>
2022-10-01 09:55:32 +0900

    added emotion reset

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Sheet from './flow';
+/** @jsxImportSource @emotion/react */
+
+import { css, Global } from "@emotion/react";
+import emotionReset from "emotion-reset";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Sheet from "./sheet";
+// import App from './App';
+// import flow from './flow';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        ${emotionReset}
+        *, *::after, *::before {
+          box-sizing: border-box;
+          -moz-osx-font-smoothing: grayscale;
+          -webkit-font-smoothing: antialiased;
+          font-smoothing: antialiased;
+        }
+      `}
+    />
     <Sheet />
+    {/* <flow /> */}
     {/* <App /> */}
   </React.StrictMode>
 );
---
+++ src/sheet.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Sheet() {
+  return <h1>sheet</h1>;
+}
+
+export default Sheet;
